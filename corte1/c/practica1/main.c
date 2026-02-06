ef40f402d811921700494b4bf2bdd50f8c0f1a94
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-02-06 15:07:34 -0500

    Add files via upload

---
+++ corte1/c/practica1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Incluye biblioteca estándar de entrada/salida
+
+int main() {
+    // printf imprime la cadena en pantalla
+    printf("¡Hola, mundo!\n");
+    return 0; // Indica que el programa finalizó correctamente
+}
+
