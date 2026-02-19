b7b742a7c689ef54fcaa55e27e8c6698352ed7ce
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-02-19 12:37:31 -0500

    Delete corte1/c/practica1/main.c

--- corte1/c/practica1/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> // Incluye biblioteca estándar de entrada/salida
-
-int main() {
-    // printf imprime la cadena en pantalla
-    printf("¡Hola, mundo!\n");
-    return 0; // Indica que el programa finalizó correctamente
-}
-
