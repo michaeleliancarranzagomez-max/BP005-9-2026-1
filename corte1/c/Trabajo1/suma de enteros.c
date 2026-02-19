97f96fa29d94320be349e858330af0116fc9dd94
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-02-19 13:08:47 -0500

    Update suma de enteros.c

--- corte1/c/Trabajo1/suma de enteros.c
+++ corte1/c/Trabajo1/suma de enteros.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h> 
+int main() {
+    int numero1, numero2, suma;
+    //poner el primer número
+    printf("escribe el primer número ");
+    scanf("%d", &numero1);
+    //poner el seúndo número
+    printf("escribe el segundo número ");
+    scanf("%d", &numero2);
+    //realización de suma
+    suma= numero1 + numero2;
+    printf("la suma de %d y de %d es %d\n", numero1, numero2, suma);
+}
