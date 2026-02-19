5bf98fbba403c6bf3c3fe2f4efad737d6e70e90e
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-02-19 15:35:22 -0500

    Update sumadefloat.c

--- corte1/c/Trabajo1/sumadefloat.c
+++ corte1/c/Trabajo1/sumadefloat.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main() {
+    //declaración de las variables 
+    float num1, num2, suma;
+    //pon los dos número
+    printf("escribe el primer numero ");
+    scanf("%f", &num1);
 
+    printf("escribe el segundo numero ");
+    scanf("%f", &num2);
+    suma = num1 + num2;
+
+    printf("la suma de %f y de %f es igual a %f", num1, num2,suma);
+return 0 ;
+}
