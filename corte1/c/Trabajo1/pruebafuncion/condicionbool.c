8e5178c7a704372a16683bc4ea6674bc9d31b07e
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-03-04 13:42:47 -0500

    Create condicionbool.c

---
+++ corte1/c/Trabajo1/pruebafuncion/condicionbool.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+int main() {
+   int x=18;
+   int y=10;
+   bool xmayorquey (x>y);
+   if (xmayorquey) {
+      printf("el número %d es mayor que %d\n", x, y);
+   }
+   return 0;
+}
