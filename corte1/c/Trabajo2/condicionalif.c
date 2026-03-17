203c4bbe5ea1912437a71bec6471e62b62f3de67
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-03-17 08:59:02 -0500

    Create condicionalif.c

---
+++ corte1/c/Trabajo2/condicionalif.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+   int numero; 
+   
+   printf("escribe el primer numero");
+   scanf("%d", &numero);
+   if (numero>0){
+      printf("el número es positivo");
+   }
+   else {
+      printf("el número es negativo");
+      }
+}
