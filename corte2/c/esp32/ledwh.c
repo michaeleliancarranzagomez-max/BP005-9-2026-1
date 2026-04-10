a93098ed740ea1320780c2d6d7fa26e2a9c93693
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-04-10 15:27:24 -0500

    Create ledwh.c

---
+++ corte2/c/esp32/ledwh.c
@@ -0,0 +1,18 @@
+const int michled = 2;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(michled, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int contador = 1;
+   while(contador <= 5){
+    digitalWrite(michled, HIGH);
+    delay(250);
+    digitalWrite(michled, LOW);
+    delay(250);
+    contador++;
+   }
+delay(1500);
+}
