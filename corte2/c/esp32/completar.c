b6fe76a876e37ffa4c67c51fc5af6581ae800fed
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-04-10 15:59:53 -0500

    Create completar.c

---
+++ corte2/c/esp32/completar.c
@@ -0,0 +1,14 @@
+const int led1 = 2;
+const int led2 = 4;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led1,OUTPUT);
+pinMode(led2,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int i; i <= 4; i++){
+  digitalWrite(led1, HIGH);
+}
+}
