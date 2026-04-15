0556c1e00ab2b1e438c633754a89e1a1d301874a
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-04-15 10:03:23 -0500

    Update and rename completar.c to fodoled.c

--- corte2/c/esp32/fodoled.c
+++ corte2/c/esp32/fodoled.c
@@ -10,5 +10,12 @@ void loop() {
   // put your main code here, to run repeatedly:
 for(int i; i <= 4; i++){
   digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  delay(3000);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
 }
+digitalWrite(led1, HIGH);
+digitalWrite(led2, LOW);
+delay(1000);
 }
