cd56812f2ceca764758e94e950a601fe34367f62
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-05-20 13:40:46 -0500

    Create arreglo1.c

---
+++ corte2/c/parcial2/arreglo1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+   int valores [5] = {8,3,15,6,10};
+   printf("valor[0]: %d\n",valores[0]);
+   printf("valor[1]: %d\n",valores[1]);
+   printf("valor[2]: %d\n",valores[2]);
+   printf("valor[3]: %d\n",valores[3]);
+   printf("valor[4]: %d\n",valores[4]);
+return 0;
+}
