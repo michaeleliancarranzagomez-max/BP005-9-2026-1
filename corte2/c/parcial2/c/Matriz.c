4dd4dec5d01a79da8bc33cbfa0c57646da2497d6
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-05-25 07:24:09 -0500

    Create Matriz.c

---
+++ corte2/c/parcial2/c/Matriz.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+   int matriz[2][3]= {{5,2,4}, {3,9,6}};
+   printf("valor [0][0] = %d\n",matriz[0][0]);
+   printf("valor [0][2] = %d\n",matriz[0][2]);
+   printf("valor [1][1] = %d\n",matriz[1][1]); 
+   return 0;
+}
