c9106ca8e1a2c35f320baac52647d542e972460f
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-05-26 11:15:47 -0500

    Update Incrementarmatr.c

--- corte2/c/parcial2/c/Incrementarmatr.c
+++ corte2/c/parcial2/c/Incrementarmatr.c
@@ -8,23 +8,22 @@ void incrementar(int m[][columnas],int filas) {
        }
     }
 }
-int main(){
-   int matriz[2][columnas]= {{5,2,4}, {3,6,8}};
-   int i,j;
-   printf("antes\n");
-   for(i = 0; i<2; i++){
+void imprimir(int matriz[][columnas],int fila){
+    int i,j;
+      for(i = 0; i<fila; i++){
       for(j = 0; j<columnas; j++){
          printf("%d, ",matriz[i][j]);
       }
       printf("\n");
-   }
+      }
+    }
+int main(){
+   int matriz[2][columnas]= {{5,2,4}, {3,6,8}};
+   int i,j;
+   printf("antes\n");
+   imprimir(matriz,2);
    incrementar(matriz,2);
    printf("despues\n");
-   for(i = 0; i<2; i++){
-      for(j = 0; j<columnas; j++){
-         printf("%d, ",matriz[i][j]);
-      }
-      printf("\n"); 
-   }
+   imprimir(matriz,2);
    return 0;
 }
