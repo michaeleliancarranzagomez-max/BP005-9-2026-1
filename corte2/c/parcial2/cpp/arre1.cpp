dc75659f4e0c48d11e8448c4a716ecdae76a65d3
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-05-26 20:11:01 -0500

    Create  arre1.cpp

---
+++ corte2/c/parcial2/cpp/arre1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(){
+    int datos[5]={1,2,3,4,5};
+    std::cout<<"dato [0] = "<<datos[0]<< std::endl;
+    std::cout<<"dato [1] = "<<datos[1]<< std::endl;
+    std::cout<<"dato [2] = "<<datos[2]<< std::endl;
+    std::cout<<"dato [3] = "<<datos[3]<< std::endl;
+    std::cout<<"dato [4] = "<<datos[4]<< std::endl;
+    return 0;
+}
