2793f5356d9468945fd32a4c2575f716a805c3cc
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-05-27 06:38:35 -0500

    Create basic2.c

---
+++ corte2/c/parcial2/c/basic2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a = 3;
+    int b = 4;
+    int suma = 0;
+    suma = a + b;
+    printf("a es %d\n", a);
+    printf("b es %d\n", b);
+    printf("suma es %d\n", suma);
+    return 0;
+}
