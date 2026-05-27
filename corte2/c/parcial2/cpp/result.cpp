3a694507572d75e06dbaf31d8e65beebafcefd0f
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-05-27 06:44:18 -0500

    Create result.cpp

---
+++ corte2/c/parcial2/cpp/result.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+    int a = 3;
+    int b = 4;
+    int suma = 0;
+    suma = a + b;
+    std::cout<<"a es"<<a<<std::endl;
+    std::cout<<"b es"<<b<<std::endl;
+    std::cout<<"la suma es "<<suma<<std::endl;
+    return 0;
+}
