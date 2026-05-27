0559b59b2824c1bdcde985d06876cd6a6b626280
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-05-26 23:33:25 -0500

    Create matriz1.cpp

---
+++ corte2/c/parcial2/cpp/matriz1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+int m[2][3] = {{1, 2, 3},{4, 5, 6}};
+std::cout << "m[0][0] = " << m[0][0] << std::endl;
+std::cout << "m[0][2] = " << m[0][2] << std::endl;
+std::cout << "m[1][1] = " << m[1][1] << std::endl;
+return 0;
+}
