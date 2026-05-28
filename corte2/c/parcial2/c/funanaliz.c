43e0deffc3eaa4743258dda769d9fdf89cee3542
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-05-27 22:41:44 -0500

    Create funanaliz.c

---
+++ corte2/c/parcial2/c/funanaliz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void analizar(int a, int b, int c, int *suma, int *mayor, int *menor){
+    *suma = a + b + c;
+    *mayor = a;
+    if(b > *mayor) *mayor = b;
+    if(c > *mayor) *mayor = c;
+    *menor = a;
+    if(b < *menor) *menor = b;
+    if(c < *menor) *menor = c;
+}
+int main(){
+    int a, b, c, suma, mayor, menor;
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &a);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &b);
+    printf("Ingrese el tercer numero: ");
+    scanf("%d", &c);
+    analizar(a, b, c, &suma, &mayor, &menor);
+    printf("La suma es: %d\n", suma);
+    printf("El mayor es: %d\n", mayor);
+    printf("El menor es: %d\n", menor);
+    return 0;
+}
