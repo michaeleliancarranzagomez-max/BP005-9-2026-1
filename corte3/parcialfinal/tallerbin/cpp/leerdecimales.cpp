4c47a8d407e973b2a183a11c666bdda8055a7b65
michaeleliancarranzagomez-max <michaeleliancarranzagomez@gmail.com>
2026-06-04 09:55:24 -0500

    Add leerdecimales.cpp to read and display temperature

---
+++ corte3/parcialfinal/tallerbin/cpp/leerdecimales.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double temperatura;
+
+    cout << "Ingrese la temperatura: ";
+    cin >> temperatura;
+    cout << temperatura << endl;
+
+return 0;
+}
